20817146e417b6e93edde786572c00a2ad62df6d
wangh <419264755@qq.com>
2019-07-23 09:06:25 +0800

    -723

---
+++ spring07_xmlioc/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/com/wh/model/Account.java,0\a\0a87b12d8e1d059773ddb937494c9b29ea115078
+a
+1src/test/java/com/wh/test/AccountServiceTest.java,c\2\c25b2b2cee31041c6aa9c910a43bb88b972a832a
+Y
+)src/main/java/com/wh/dao/IAccountDao.java,1\1\11c811879160e41943aba1af2941ff5081c9e27b
+a
+1src/main/java/com/wh/service/IAccountService.java,2\8\28c7bd5546550bae918b1e2337404b2679ae97c4
+i
+9src/main/java/com/wh/service/impl/AccountServiceImpl.java,b\0\b008692fe61f50081d9be25e21bff47c0ea88bdc
+a
+1src/main/java/com/wh/dao/impl/AccountDaoImpl.java,5\1\516fa8c7b1559ef3ded289f9133d2838befe3906
---
+++ spring07_xmlioc/src/main/java/com/wh/service/impl/AccountServiceImpl.java
@@ -0,0 +1,36 @@
+package com.wh.service.impl;
+
+import com.wh.dao.IAccountDao;
+import com.wh.model.Account;
+import com.wh.service.IAccountService;
+
+import java.util.List;
+
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public List<Account> findAllAccount() {
+        return accountDao.findAllAccount();
+    }
+
+    public Account findAccountById(Integer id) {
+        return accountDao.findAccountById(id);
+    }
+
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+    }
+
+    public void updateAccount(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    public void deleteAccount(Integer id) {
+        accountDao.deleteAccount(id);
+    }
+}
