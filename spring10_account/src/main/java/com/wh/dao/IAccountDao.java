eec4c5cb8fa8fb115b2d6001bc0a376d1ddd4b76
wangh <419264755@qq.com>
2019-07-26 16:45:24 +0800

    0726

---
+++ spring10_account/src/main/java/com/wh/dao/IAccountDao.java
@@ -0,0 +1,50 @@
+package com.wh.dao;
+
+import com.wh.model.Account;
+
+import java.util.List;
+
+/**
+ * 账户的持久层
+ */
+public interface IAccountDao {
+
+    /**
+     * 查询所有
+     * @return
+     */
+    List<Account> findAllAccount();
+
+    /**
+     * 查询一个
+     * @return
+     */
+    Account findAccountById(Integer id);
+
+    /**
+     * 保存
+     * @param account
+     */
+    void saveAccount(Account account);
+
+    /**
+     * 更新
+     * @param account
+     */
+    void updateAccount(Account account);
+
+    /**
+     * 删除
+     * @param id
+     */
+    void deleteAccount(Integer id);
+
+    /**
+     * 根据名称查询账户
+     * @param accountName
+     * @return 如果有唯一的结果就返回，如果没有结果就返回null
+     *          如果结果集超过一个就抛异常
+     */
+    Account findAccountByName(String accountName);
+
+}
